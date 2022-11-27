1e74648d31070ce2e1ef159983ee145cbae673cf
ibrahimshn <ibrahimsahinaws@gmail.com>
2022-11-27 05:49:18 -0800

    ES-1 Enable Async for ExchangeLogListener

--- src/main/java/com/akinon/exchange/listener/ExchangeLogListener.java
+++ src/main/java/com/akinon/exchange/listener/ExchangeLogListener.java
@@ -8,9 +8,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
 
 @Slf4j
+@EnableAsync
 @RequiredArgsConstructor
 @Component
 public class ExchangeLogListener {
